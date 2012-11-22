33a80f5f55b9899a511d8b949f18ed607d2b951b
Stefaan Lippens <soxofaan@gmail.com>
2012-11-22 10:48:48 +0100

    more prototyping and experimening

--- js/evinz.js
+++ js/evinz.js
@@ -1,37 +1,65 @@
-(function($,d3) {
+(function(window, $, d3) {
 
-	// Get data from text field
-	var text = $('#input-textarea').val();
-	var lines = text.split('\n');
-	var data = [];
-	for (var i = 0; i < lines.length; i++) {
-		var value = parseInt(lines[i], 10);
-		if (!isNaN(value)) {
-			data.push(value);
+	var get_data = function() {
+		// Get data from text field
+		var text = $('#input-textarea').val();
+		var lines = text.split('\n');
+		var data = [];
+		for (var i = 0; i < lines.length; i++) {
+			var value = parseInt(lines[i], 10);
+			if (!isNaN(value)) {
+				data.push(value);
+			}
 		}
-	}
+		return data;
+	};
+
+	window.evinz = function() {
+
+		var data = get_data();
+
+		// Create graph
+		var svg = d3.select("#evinz-plot");
+		var plot_width = svg.attr('width');
+		var plot_height = svg.attr('height');
+		var plot_padding = 10;
+
+		var x = d3.scale.linear()
+			.domain([d3.min(data), d3.max(data)])
+			.range([plot_padding, plot_width - plot_padding]);
 
 
-	// Create graph
-	var plot_width = 800;
-	var plot_height = 400;
-	var plot_padding = 10;
-	var svg = d3.select("body").append("svg")
-		.attr("class", "chart")
-		.attr("width", plot_width)
-		.attr("height", plot_height);
+		// Dots
+		var circles = svg.selectAll("circle").data(data, function(d){return d;});
+		circles.transition()
+			.duration(500)
+			.attr('cx', x);
+		// Entering dots
+		circles.enter().append('circle')
+			.attr('class', 'event')
+			.attr('cx', x)
+			.attr('cy', plot_height / 2)
+			.transition()
+			.delay(function(d, i) { return 50 * i;})
+			.attr('r', 4);
+		// Exiting dots
+		var exit = circles.exit().remove();
 
-	var x = d3.scale.linear()
-		.domain([d3.min(data), d3.max(data)])
-		.range([plot_padding, plot_width - plot_padding]);
+		var lines = svg.selectAll("line").data(data);
+		lines.transition()
+			.attr('x1', x).attr('y1', plot_height/2)
+			.attr('x2', x).attr('y2', plot_height * 0.25);
+		// Entering lines
+		lines.enter().append('line')
+			.attr('class', 'event')
+			.attr('x1', x).attr('y1', plot_height / 2)
+			.attr('x2', x).attr('y2', plot_height / 2)
+			.transition()
+			.delay(function(d, i) { return 100 * i;})
+			.attr('y2', plot_height* 0.25);
+		// Exiting lines
+		var exit = circles.exit().remove();
 
+	};
 
-	var circles = svg.selectAll("circle").data(data);
-
-	var enter = circles.enter().append('circle')
-		.attr('cx', x)
-		.attr('cy', plot_height / 2)
-		.attr('r', 4)
-		.attr('class', 'event');
-
-})(jQuery, d3);
+})(window, jQuery, d3);

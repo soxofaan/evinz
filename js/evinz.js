a51d8ef132cacc8e7b25b79e9849d204dcd7b0d1
Stefaan Lippens <soxofaan@gmail.com>
2012-11-23 23:32:09 +0100

    finetuned combined plot of dots and histogram

--- js/evinz.js
+++ js/evinz.js
@@ -30,62 +30,66 @@
 		var svg_height = svg.attr('height');
 
 		// Set up plot area.
-		var plot_margin = {top: 10, bottom: 40, left: 20, right: 20};
-		var plot_padding = 30;
+		var plot_margin = {top: 30, bottom: 10, left: 40, right: 20, vertical_padding: 10};
 		var plot_width = svg_width - plot_margin.left - plot_margin.right;
 		var plot_height = svg_height - plot_margin.top - plot_margin.bottom;
-		var plot = svg.append('g')
+		var dot_plot_height = 40;
+		var histogram_plot_top = plot_margin.vertical_padding + dot_plot_height + plot_margin.vertical_padding;
+		var histogram_plot_height = plot_height - histogram_plot_top;
+
+		var plot_area = svg.append('g')
 			.attr('class', 'plot-area')
-			.attr("transform", "translate(" + plot_margin.top + "," + plot_margin.left + ")");
+			.attr("transform", "translate(" + plot_margin.left + "," + plot_margin.top + ")");
 
 		// X scale: range based on data time stamps
+		dmin = d3.min(data);
+		dmax = d3.max(data);
 		var x = d3.time.scale()
-			.domain([d3.min(data), d3.max(data)])
+			.domain([1.02 * dmin - 0.02 * dmax, 1.02 * dmax - 0.02 * dmin])
 			.range([0, plot_width]);
-		// Y scale: based on data (counts). TODO
-		var y = d3.scale.linear()
-			.domain([0, 1])
-			.range([plot_height, 0]);
 
 		// X axis
 		var axis = d3.svg.axis()
 			.scale(x)
-			.orient('bottom')
+			.orient('top')
 			.ticks(10)
-			.tickSize(10);
-		plot.append('g')
+			.tickSize(-plot_height);
+		plot_area.append('g')
 			.attr('class', 'axis')
-			.attr("transform", "translate(0, " + (plot_height) + ")")
 			.call(axis);
 
 		// Event dot plot
-		var dot_plot = plot.append('g').attr('class', 'dot-plot');
+		var dot_plot = plot_area
+			.append('g')
+			.attr('class', 'dot-plot')
+			.attr('transform', 'translate(0,' + plot_margin.vertical_padding + ')');
+		// Y scale: based on data (counts). TODO
+		var dot_plot_yscale = d3.scale.linear()
+			.domain([0, 1])
+			.range([dot_plot_height, 0]);
 
 		// First weight/count lines
 		var lines = dot_plot.selectAll("line.event").data(data);
 		lines
-			.attr('x1', x).attr('y1', y(0))
-			.attr('x2', x).attr('y2', y(1));
+			.attr('x1', x).attr('y1', dot_plot_yscale(0))
+			.attr('x2', x).attr('y2', dot_plot_yscale(1));
 		// Entering lines
 		lines.enter().append('line')
 			.attr('class', 'event')
-			.attr('x1', x).attr('y1', y(0))
-			.attr('x2', x).attr('y2', y(0))
-			.transition()
-			.attr('y2', y(1));
+			.attr('x1', x).attr('y1', dot_plot_yscale(0))
+			.attr('x2', x).attr('y2', dot_plot_yscale(1));
 		// Exiting lines
 		lines.exit().remove();
 
 		// Event dots
 		var dots = dot_plot.selectAll("circle.event").data(data);
 		dots
-			.attr('cx', x).attr('cy', y(0));
+			.attr('cx', x).attr('cy', dot_plot_yscale(0));
 		// Entering dots
 		dots.enter().append('circle')
 			.attr('class', 'event')
-			.attr('cx', x).attr('cy', y(0))
-			.transition()
-			.attr('r', 4);
+			.attr('cx', x).attr('cy', dot_plot_yscale(0))
+			.attr('r', 3);
 		// Exiting dots
 		dots.exit().remove();
 
@@ -94,19 +98,24 @@
 		var histogram_builder = d3.layout.histogram();
 		// How to transform date to a number, so histogram can do it's thing
 		histogram_builder.value(function(date) { return date.getTime(); });
-		histogram_builder.bins(32);
+		histogram_builder.bins(64);
 		// Feed the data to build the histogram.
 		histogram = histogram_builder(data);
 
-
+		// Scale for the histogram data
 		histogram_yscale = d3.scale.linear()
-			.domain([0, d3.max(histogram, function(d) { return d.y; })])
-			.range([plot_height, 0]);
+			.domain([0, d3.max(histogram, function(d) { return d.y; }) + 1])
+			.range([histogram_plot_height, 0]);
 
-		// Histrogram plot
-		var histogram_plot = plot.insert('g', '.dot-plot')
-			.attr('class', 'histogram-plot');
-		var bars = histogram_plot.selectAll('.bar').data(histogram);
+		// Histogram plot
+		var histogram_plot = plot_area.append('g')
+			.attr('class', 'histogram-plot')
+			.attr('transform', 'translate(0,' + histogram_plot_top + ')');
+		var bars = histogram_plot
+			.append('g')
+			.attr('class', 'bars')
+			.selectAll('.bar')
+			.data(histogram);
 		bars.enter().append('rect');
 		bars.exit().remove();
 		bars.attr('class', 'bar')
@@ -114,6 +123,15 @@
 			.attr('x', function(d) { return x(d.x); })
 			.attr('y', function(d) { return histogram_yscale(d.y); })
 			.attr('height', function(d) { return histogram_yscale(0) - histogram_yscale(d.y); });
+
+		// Y axis for histogram
+		var histogram_yaxis = d3.svg.axis()
+			.scale(histogram_yscale)
+			.orient('left')
+			.tickSize(-plot_width);
+		histogram_plot.insert('g', '.bars')
+			.attr('class', 'axis')
+			.call(histogram_yaxis);
 
 
 	};

748219ce3bd0eea4778275ec92402d1d96698e05
Stefaan Lippens <soxofaan@gmail.com>
2012-11-23 02:20:29 +0100

    Got axis stuff working finally, more grouping in svg

--- js/evinz.js
+++ js/evinz.js
@@ -3,12 +3,15 @@
 	var get_data = function() {
 		// Get data from text field
 		var text = $('#input-textarea').val();
+		var format = $('#input-time-format').val();
+		format = d3.time.format(format);
+
 		var lines = text.split('\n');
 		var data = [];
 		for (var i = 0; i < lines.length; i++) {
-			var value = parseInt(lines[i], 10);
-			if (!isNaN(value)) {
-				data.push(value);
+			var t = format.parse(lines[i]);
+			if (t) {
+				data.push(t);
 			}
 		}
 		return data;
@@ -16,50 +19,83 @@
 
 	window.evinz = function() {
 
+		// Get data.
+		// TODO: pass this as argument.
 		var data = get_data();
 
-		// Create graph
+		// Get SVG canvas to use
+		// TODO: pass this as argument?
 		var svg = d3.select("#evinz-plot");
-		var plot_width = svg.attr('width');
-		var plot_height = svg.attr('height');
-		var plot_padding = 10;
+		var svg_width = svg.attr('width');
+		var svg_height = svg.attr('height');
 
-		var x = d3.scale.linear()
+		// Set up plot area.
+		var plot_margin = {top: 10, bottom: 40, left: 20, right: 20};
+		var plot_padding = 30;
+		var plot_width = svg_width - plot_margin.left - plot_margin.right;
+		var plot_height = svg_height - plot_margin.top - plot_margin.bottom;
+		var plot = svg.append('g')
+			.attr('class', 'plot-area')
+			.attr("transform", "translate(" + plot_margin.top + "," + plot_margin.left + ")");
+
+		// X scale: range based on data time stamps
+		var x = d3.time.scale()
 			.domain([d3.min(data), d3.max(data)])
-			.range([plot_padding, plot_width - plot_padding]);
+			.range([0, plot_width]);
+		// Y scale: based on data (counts). TODO
+		var y = d3.scale.linear()
+			.domain([0, 1])
+			.range([plot_height, 0]);
 
+		// X axis
+		var axis = d3.svg.axis()
+			.scale(x)
+			.orient('bottom')
+			.ticks(10)
+			.tickSize(10);
+		plot.append('g')
+			.attr('class', 'axis')
+			.attr("transform", "translate(0, " + (plot_height) + ")")
+			.call(axis);
 
-		// Dots
-		var circles = svg.selectAll("circle").data(data, function(d){return d;});
-		circles.transition()
-			.duration(500)
-			.attr('cx', x);
-		// Entering dots
-		circles.enter().append('circle')
-			.attr('class', 'event')
-			.attr('cx', x)
-			.attr('cy', plot_height / 2)
-			.transition()
-			.delay(function(d, i) { return 50 * i;})
-			.attr('r', 4);
-		// Exiting dots
-		var exit = circles.exit().remove();
+		// Event dot plot
+		var dot_plot = plot.append('g').attr('class', 'dot-plot');
 
-		var lines = svg.selectAll("line").data(data);
-		lines.transition()
-			.attr('x1', x).attr('y1', plot_height/2)
-			.attr('x2', x).attr('y2', plot_height * 0.25);
+		// First weight/count lines
+		var lines = dot_plot.selectAll("line.event").data(data);
+		lines
+			.attr('x1', x).attr('y1', y(0))
+			.attr('x2', x).attr('y2', y(1));
 		// Entering lines
 		lines.enter().append('line')
 			.attr('class', 'event')
-			.attr('x1', x).attr('y1', plot_height / 2)
-			.attr('x2', x).attr('y2', plot_height / 2)
+			.attr('x1', x).attr('y1', y(0))
+			.attr('x2', x).attr('y2', y(0))
 			.transition()
-			.delay(function(d, i) { return 100 * i;})
-			.attr('y2', plot_height* 0.25);
+			.attr('y2', y(1));
 		// Exiting lines
-		var exit = circles.exit().remove();
+		lines.exit().remove();
+
+		// Event dots
+		var dots = dot_plot.selectAll("circle.event").data(data);
+		dots
+			.attr('cx', x).attr('cy', y(0));
+		// Entering dots
+		dots.enter().append('circle')
+			.attr('class', 'event')
+			.attr('cx', x).attr('cy', y(0))
+			.transition()
+			.attr('r', 4);
+		// Exiting dots
+		dots.exit().remove();
+
+
+		var range = d3.time.day.range(d3.min(data), d3.max(data));
+		console.log(range);
+
 
 	};
 
 })(window, jQuery, d3);
+
+evinz();
